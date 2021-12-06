f10b3a9b3b294c3fb80d162633611a4130a70271
Daepoid <daepoid@gmail.com>
2021-12-07 00:25:17 +0900

    Add Files

---
+++ src/test/java/daepoid/stockManager/StockManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package daepoid.stockManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
