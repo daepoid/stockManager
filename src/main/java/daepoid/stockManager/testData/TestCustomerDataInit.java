e7a036598d557dfd3c6564ad9681e14ae040a104
daepoid <daepoid@gmail.com>
2022-05-06 10:04:32 +0900

    수정중

--- src/main/java/daepoid/stockManager/testData/TestCustomerDataInit.java
+++ src/main/java/daepoid/stockManager/testData/TestCustomerDataInit.java
@@ -1,54 +1,54 @@
-//package daepoid.stockManager.testData;
-//
-//import daepoid.stockManager.domain.member.GradeType;
-//import daepoid.stockManager.domain.order.Cart;
-//import daepoid.stockManager.domain.order.Customer;
-//import daepoid.stockManager.service.CustomerService;
-//
-//import lombok.RequiredArgsConstructor;
-//
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Component;
-//
-//import javax.annotation.PostConstruct;
-//import java.util.HashMap;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class TestCustomerDataInit {
-//
-//    private final CustomerService customerService;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    @PostConstruct
-//    public void init() {
-//
-//        customerService.saveCustomer(Customer.builder()
-//                .loginId("table001")
-//                .userName("customer name or table name1")
-//                .password(passwordEncoder.encode("123"))
-//                .gradeType(GradeType.CUSTOMER)
-//                .tableNumber("1")
-//                .cart(new Cart(new HashMap<>()))
-//                .build());
-//
-//        customerService.saveCustomer(Customer.builder()
-//                .loginId("table002")
-//                .userName("customer name or table name2")
-//                .password(passwordEncoder.encode("123"))
-//                .gradeType(GradeType.CUSTOMER)
-//                .tableNumber("2")
-//                .cart(new Cart(new HashMap<>()))
-//                .build());
-//
-//        customerService.saveCustomer(Customer.builder()
-//                .loginId("table003")
-//                .userName("customer name or table name3")
-//                .password(passwordEncoder.encode("123"))
-//                .gradeType(GradeType.CUSTOMER)
-//                .tableNumber("3")
-//                .cart(new Cart(new HashMap<>()))
-//                .build());
-//    }
-//
-//}
+package daepoid.stockManager.testData;
+
+import daepoid.stockManager.domain.member.GradeType;
+import daepoid.stockManager.domain.order.Cart;
+import daepoid.stockManager.domain.order.Customer;
+import daepoid.stockManager.service.CustomerService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+
+@Component
+@RequiredArgsConstructor
+public class TestCustomerDataInit {
+
+    private final CustomerService customerService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void init() {
+
+        customerService.saveCustomer(Customer.builder()
+                .loginId("table001")
+                .userName("customer name or table name1")
+                .password(passwordEncoder.encode("123"))
+                .gradeType(GradeType.CUSTOMER)
+                .tableNumber("1")
+                .cart(new Cart(new HashMap<>()))
+                .build());
+
+        customerService.saveCustomer(Customer.builder()
+                .loginId("table002")
+                .userName("customer name or table name2")
+                .password(passwordEncoder.encode("123"))
+                .gradeType(GradeType.CUSTOMER)
+                .tableNumber("2")
+                .cart(new Cart(new HashMap<>()))
+                .build());
+
+        customerService.saveCustomer(Customer.builder()
+                .loginId("table003")
+                .userName("customer name or table name3")
+                .password(passwordEncoder.encode("123"))
+                .gradeType(GradeType.CUSTOMER)
+                .tableNumber("3")
+                .cart(new Cart(new HashMap<>()))
+                .build());
+    }
+
+}
